c1d4fc38ae4ccb0d2e2111238ceca1b18255586d
Nancy W <159850766+nancying8@users.noreply.github.com>
2024-12-12 23:47:54 -0800

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";  // import React to use JSX and component features 
-import Banner from "../components/Banner";  // import Banner component
+import Banner from "../components/Banner"; // import Banner component
 import Content from "../components/Content";  // import Content component  
 
 
